d6be3eef31885444ef7b6c7e81d0e5359e8045f9
Sudarshan Waghmare <sudarshanwaghmare143@gmail.com>
2024-12-10 21:21:53 +0530

    Create: Folder +Item

--- FolderCreate.cxx
+++ FolderCreate.cxx
@@ -23,23 +23,26 @@ int ITK_user_main(int argc,	char* argv[]) {
 	tag_t create_input = NULLTAG;
 	tag_t new_folder = NULLTAG;
 	
-	status = TCTYPE_ask_type("Folder", &type);
-	
-	status = TCTYPE_construct_create_input(type, &create_input);
-	
-	status = AOM_set_value_string(create_input, "object_name", "Folder by ITK_1");
-		
+	status = TCTYPE_ask_type("Folder", &type);	
+	status = TCTYPE_construct_create_input(type, &create_input);	
+	status = AOM_set_value_string(create_input, "object_name", "Folder by ITK_AAA");		
 	status = TCTYPE_create_object(create_input, &new_folder);	
-
 	status = AOM_save_with_extensions(new_folder);	
-
 	status = SA_find_user2("infodba", &Fuser);
-
-	status = SA_ask_user_home_folder(Fuser, &Uhome_folder);
-	
+	status = SA_ask_user_home_folder(Fuser, &Uhome_folder);	
 	status = FL_insert(Uhome_folder, new_folder, 999);
-
 	status = AOM_save_with_extensions(Uhome_folder);
+
+	tag_t NewItem = NULLTAG;
+	status = TCTYPE_find_type("A3BHMakePart", "A3BHMakePart", &type);
+	status = TCTYPE_construct_create_input(type, &create_input);
+	status = AOM_set_value_string(create_input, "object_name", "BH Item created by ITk_AAA");
+	status = AOM_set_value_string(create_input, "a3MakeBuy", "Make");
+	status = TCTYPE_create_object(create_input, &NewItem);
+	status = AOM_save_with_extensions(NewItem); //save item
+	status = FL_insert(new_folder, NewItem, 999);
+	status = AOM_save_with_extensions(new_folder);
+
 
 	status = ITK_exit_module(true); 
 
